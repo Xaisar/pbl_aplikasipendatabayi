ba29ed9d2f434ed85e2ca828f3125cf3ceef8a52
Arilex <110915525+Xaisar@users.noreply.github.com>
2022-11-28 15:13:44 +0700

    Ariel

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:aplikasipendatabayi/app/modules/index/views/index_view.dart';
+import 'package:aplikasipendatabayi/app/views/views/development_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -8,8 +8,8 @@ void main() {
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
     title: "Puskemas Kabat",
-    initialRoute: Routes.INDEX,
-    home: const IndexView(),
-    getPages: AppPages.routes,
+    // initialRoute: Routes.INDEX,
+    // getPages: AppPages.routes,
+    home: const DevelopmentView(),
   ));
 }

dfa28b23acb4cd971c17090532a7cd222df17ca8
Nandaayuputriashari <nandaayuputriashari@gmail.com>
2022-11-30 13:52:51 +0700

    nanda

--- lib/app/modules/riwayat/views/riwayat_view.dart
+++ lib/app/modules/riwayat/views/riwayat_view.dart
@@ -9,14 +9,56 @@ class RiwayatView extends GetView<RiwayatController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('RiwayatView'),
-        centerTitle: true,
+        backgroundColor: Color.fromARGB(207, 199, 206, 209),
+        leading: Image.asset("assets/icons/panahback.png"),
+        title:
+            Text('Detail Riwayat Bayi', style: TextStyle(color: Colors.black)),
+        actions: [
+          SizedBox(
+            width: 100,
+            height: 50,
+            child: ElevatedButton(
+              onPressed: () {
+                print('ini done');
+              },
+              child: new Text('Edit'),
+              style: TextButton.styleFrom(
+                backgroundColor: Color(0xFF1CAE81),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+          ),
+          // SizedBox(
+          //   child: Container(
+          //     width: 100,
+          //     height: 26,
+          //     child: TextButton(
+          //       style: TextButton.styleFrom(
+          //           backgroundColor: Color(0xFF1CAE81),
+          //           shape: RoundedRectangleBorder(
+          //             borderRadius: BorderRadius.circular(20),
+          //           )),
+          //       onPressed: () {},
+          //       child: Text(
+          //         "Edit",
+          //         style: TextStyle(
+          //           color: Color(0xffffffff),
+          //         ),
+          //       ),
+          //     ),
+          //   ),
+          // ),
+        ],
       ),
-      body: Center(
-        child: Text(
-          'RiwayatView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Stack(
+        children: [
+          Container(
+            width: 50,
+            child: const Text("NIK Bayi", style: TextStyle(fontSize: 15)),
+          )
+        ],
       ),
     );
   }

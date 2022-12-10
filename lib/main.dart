5966761ef339c940c852faf16159dfdb44056fb4
Nandaayuputriashari <nandaayuputriashari@gmail.com>
2022-12-10 13:39:27 -0800

    nanda

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:aplikasipendatabayi/app/modules/login/views/login_view.dart';
+import 'package:aplikasipendatabayi/app/modules/riwayat/views/riwayat_view.dart';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -11,6 +12,6 @@ void main() {
     title: "Puskemas Kabat",
     // initialRoute: Routes.INDEX,
     // getPages: AppPages.routes,
-    home: LoginView(),
+    home: RiwayatView(),
   ));
 }
